b306d62d31b6494823f8978251ab8cd080533796
Phillip Caudell <phill@foundersfactory.com>
2018-05-21 13:26:28 +0100

    Initial Commit

---
+++ Drive/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Drive
+//
+//  Created by Phillip Caudell on 21/05/2018.
+//  Copyright © 2018 Phillip Caudell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Drive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Drive
+//
+//  Created by Phillip Caudell on 21/05/2018.
+//  Copyright © 2018 Phillip Caudell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
