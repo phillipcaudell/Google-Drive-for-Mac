e4cb24ac0541da8007378fe842a56e17f4e2c9f1
Sam Houghton <sam@foundersfactory.co>
2018-05-25 11:59:40 +0100

    Adds open location to open documents from a link

--- Drive/AppDelegate.swift
+++ Drive/AppDelegate.swift
@@ -28,6 +28,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.makeKeyAndOrderFront(nil)
     }
     
+    @IBAction func handleOpenLocation(_ sender: Any) {
+        
+        let a = NSAlert()
+        a.messageText = "Enter Link Below"
+        a.addButton(withTitle: "Go")
+        a.addButton(withTitle: "Cancel")
+        
+        let inputTextField = NSTextField(frame: NSRect(x: 0, y: 0, width: 300, height: 24))
+        inputTextField.placeholderString = "drive.google.com"
+        a.accessoryView = inputTextField
+        
+        a.beginSheetModal(for: self.window!, completionHandler: { (modalResponse) -> Void in
+            if modalResponse == NSApplication.ModalResponse.alertFirstButtonReturn {
+                let enteredString = inputTextField.stringValue
+                let url = URL(string: enteredString)
+                if let url = url {
+                    self.openWindow(url: url)
+                }
+            }
+        })
+    }
+    
     @IBAction func handleNewDoc(_ sender: Any) {
         let url = URL(string: "https://docs.google.com/document/create?hl=en")
         openWindow(url: url!)
